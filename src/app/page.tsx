82d4d334407c2ece30200d173b23533a76b64046
Maawa Khalid <maaawa3@gmail.com>
2025-06-10 02:55:17 +0500

    modified:   src/app/globals.css modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 export default function Home() {
   return (
-    <div><h1 className="border-4 border-blue-800 bg-red-600 text-green-600">
+    <div><h1 className="border-4 border-blue-800 bg-gradient-to-r from-indigo-500 to-teal-400 text-green-600">
+
       hey meow
     </h1>
     <h1 className="text-yellow-600">
       hey meow
     </h1></div>
+    
+
     
   )
 }
